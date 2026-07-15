1ddc60050097e9ef93f7a654ecfe284d1c395ffe
ZahraMozafari23 <95679906+ZahraMozafari23@users.noreply.github.com>
2026-07-15 17:55:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def send_message(text):
         )
             print("Telegram Status:", response.status_code)
             print(response.text)
+        
     except Exception as e:
         print("Telegram Error:", e)
 
